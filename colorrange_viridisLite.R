af2a54780fed140b2fb19704b4b968ec1a24e9bb
Dominique <hugooperez10@gmail.com>
2018-12-29 17:08:43 +0100

    Update colorrange_viridisLite.R

--- colorrange_viridisLite.R
+++ colorrange_viridisLite.R
@@ -1,8 +1,18 @@
-
 library(viridisLite)
 
-pie(rep(1,10), col = viridisLite::plasma(10), lty = 3,main = "viridisLite::plasma")
-pie(rep(1,10), col = viridisLite::cividis(10), lty = 3,main = "viridisLite::cividis")
-pie(rep(1,10), col = viridisLite::inferno(10), lty = 3,main = "viridisLite::inferno")
-pie(rep(1,10), col = viridisLite::magma(10), lty = 3,main = "viridisLite::magma")
-pie(rep(1,10), col = viridisLite::viridis(10), lty = 3,main = "viridisLite::viridis")
+dev.off()
+
+split.screen(figs = c(2,1))
+split.screen(figs = c(1,2), screen = 1)
+split.screen(figs = c(1,3), screen = 2)
+
+screen(3)
+pie(rep(1,10), col = viridisLite::plasma(10), lty = 3,radius = 1,main = "viridisLite::plasma")
+screen(4)
+pie(rep(1,10), col = viridisLite::cividis(10), lty = 3,radius = 1,main = "viridisLite::cividis")
+screen(5)
+pie(rep(1,10), col = viridisLite::inferno(10), lty = 3,radius = 1,main = "viridisLite::inferno")
+screen(6)
+pie(rep(1,10), col = viridisLite::magma(10), lty = 3,radius = 1,main = "viridisLite::magma")
+screen(7)
+pie(rep(1,10), col = viridisLite::viridis(10), lty = 3,radius = 1,main = "viridisLite::viridis")
